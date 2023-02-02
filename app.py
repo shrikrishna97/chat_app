b0ee65a5dcee63239ff8a0524bad6f46d855998d
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2023-02-02 21:12:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Store messages in memory
+messages = []
+
+st.title("Streamlit Chat App")
+
+# Create a chat interface
+username = st.text_input("Enter your username:")
+message = st.text_input("Enter message:")
+
+# Send message
+if st.button("Send"):
+    messages.append((username, message))
+    st.write("Sent!")
+
+# Display messages
+for msg in messages:
+    st.write("[{}]: {}".format(msg[0], msg[1]))
